a3b82f83fd8a2821a7c7a515ed4b46be1c62431e
ImUrX <urielfontan2002@gmail.com>
2021-09-23 14:09:15 -0300

    handle saucenao

--- src/commands/Weeb/sauce.ts
+++ src/commands/Weeb/sauce.ts
@@ -1,8 +1,8 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction, BaseCommandInteraction, MessageEmbed, MessageAttachment } from "discord.js";
+import { CommandInteraction, BaseCommandInteraction, MessageEmbed } from "discord.js";
 import fetch from "node-fetch";
 import { stripIndent } from "common-tags";
-import { randomSadEmoji } from "../../util/util.js";
+import { getKawaiiLink, randomSadEmoji } from "../../util/util.js";
 import Command from "../../lib/Command.js";
 import auth from "../../../auth.json";
 import EmbedList from "../../util/EmbedList.js";
@@ -32,9 +32,8 @@ export default class SauceCommand extends Command {
         const nsfw = interaction.channel && "nsfw" in interaction.channel && interaction.channel.nsfw;
         const json = await fetch(stripIndent`
         https://saucenao.com/search.php?db=999&output_type=2&numres=5${nsfw ? "" : "&hide=3"}&api_key=${auth.saucenao}&url=${encodeURIComponent(url)}`
-        ).then(res => res.json() as Promise<SauceNAOData>);
-        if(json.results[0].header.index_id === 34) {
-        }
+        ).then(res => res.json() as Promise<SauceNAOData<unknown>>);
+
         if(json.header.status > 0 && !json.results.length) {
             return interaction.reply({ content: `It seems SauceNAO is having some problems (code ${json.header.status})`, ephemeral: true });
         }
@@ -46,34 +45,107 @@ export default class SauceCommand extends Command {
         }
         await interaction.deferReply();
         const embedList = new EmbedList({ time: 30000 });
-        for(let i = 0; i < 3; i++) {
+        for(let i = 0; i < json.results.length; i++) {
             embedList.add(
-                new MessageEmbed()
-                    .setTitle("this is an embed")
-                    .setDescription(i.toString())
+                await SauceCommand.createEmbed(json.results[i])
             );
         }
         await embedList.send(interaction);
     }
 
-    static async createEmbed(res: SauceNAOData, index: number): Promise<{
-        embed: MessageEmbed,
-        attachment: MessageAttachment
-    }> {
-        if(res.results[index].header.index_id === 34) {
-            res.results[index]
+    static async createEmbed(data: SauceNAOResult<unknown>): Promise<MessageEmbed> {
+        const res = new MessageEmbed()
+            .setImage(await getKawaiiLink(data.header.thumbnail))
+            .setDescription(`Similarity ${data.header.similarity}%`)
+            .setFooter(data.header.index_name)
+            .setColor("PURPLE");
+        if(data.data.ext_urls && data.data.ext_urls.length > 0) {
+            res.setURL(data.data.ext_urls[0]);
         }
+        switch(data.header.index_id) {
+            case 34: { // no deep type guard currently for discrimating unions
+                const tmp = data as SauceNAOResult<34>;
+                res.setAuthor(tmp.data.author_name, undefined, tmp.data.author_url)
+                    .setTitle(tmp.data.title);
+                break;
+            }
+            case 5:
+            case 6: {
+                const tmp = data as SauceNAOResult<6>;
+                res.setAuthor(tmp.data.member_name, undefined, `https://www.pixiv.net/users/${tmp.data.member_id}`)
+                    .setTitle(tmp.data.title);
+                break;
+            }
+            case 21: {
+                const tmp = data as SauceNAOResult<21>;
+                if(tmp.data.part) res.addField("Part:", tmp.data.part, true);
+                res.setTitle(tmp.data.source)
+                    .addField("Timestamp:", tmp.data.est_time, true);
+                break;
+            }
+            case 40: {
+                const tmp = data as SauceNAOResult<40>;
+                res.setAuthor(tmp.data.author_name, undefined, tmp.data.author_url)
+                    .setTitle(tmp.data.title);
+                break;
+            }
+            case 41: {
+                const tmp = data as SauceNAOResult<41>;
+                res.setTitle(`Tweet by @${tmp.data.twitter_user_handle}`)
+                    .setTimestamp(new Date(tmp.data.created_at));
+                break;
+            }
+            case 38: {
+                const tmp = data as SauceNAOResult<38>;
+                res.setTitle(tmp.data.source)
+                    .setAuthor(tmp.data.creator.join(" & "));
+                break;
+            }
+            case 31: {
+                const tmp = data as SauceNAOResult<31>;
+                res.setTitle(tmp.data.title)
+                    .setAuthor(tmp.data.member_name, undefined, `https://bcy.net/u/${tmp.data.member_link_id}`);
+                break;
+            }
+            case 42: {
+                const tmp = data as SauceNAOResult<42>;
+                res.setTitle(tmp.data.title)
+                    .setAuthor(tmp.data.author_name, undefined, tmp.data.author_url);
+                break;
+            }
+            case 35: {
+                const tmp = data as SauceNAOResult<35>;
+                res.setTitle(`Toot by ${tmp.data.pawoo_user_username}`);
+                break;
+            }
+            case 8: {
+                const tmp = data as SauceNAOResult<8>;
+                res.setTitle(tmp.data.title)
+                    .setAuthor(tmp.data.member_name, undefined, `https://seiga.nicovideo.jp/user/illust/${tmp.data.member_id}`);
+                break;
+            }
+            case 27: {
+                const tmp = data as SauceNAOResult<27>;
+                res.setTitle(tmp.data.source)
+                    .setAuthor(tmp.data.creator);
+                break;
+            }
+            default:
+                console.error(`Don't know where this SauceNAO index is from: ${JSON.stringify(data)}`);
+                res.setTitle("¿?");
+        }
+        return res;
     }
 }
 
-export type SauceNAOData = {
+export type SauceNAOData<T> = {
     header: {
         status: number
     },
-    results: SauceNAOResult[]
+    results: SauceNAOResult<T>[]
 }
 
-type SauceNAOResult = {
+type SauceNAOResult<T extends keyof DataType | unknown> = {
     header: {
         similarity: string,
         thumbnail: string,
@@ -83,7 +155,7 @@ type SauceNAOResult = {
     },
     data: {
         ext_urls?: string[],
-    }
+    } & (T extends keyof DataType ? DataType[T] : unknown);
 }
 
 export type DataType = {
