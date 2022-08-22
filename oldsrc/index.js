227a4b374f1f97a44318b03c9f0f1ea1d7edd971
ImUrX <urielfontan2002@gmail.com>
2022-08-22 20:16:42 -0300

    delete old src

--- oldsrc/index.js
+++
@@ -1,7 +0,0 @@
-const Client = require("./lib/SweetieClient");
-const config = require("../config");
-const auth = require("../auth.json");
-
-const client = new Client(config.client);
-
-client.login(auth.discord);
